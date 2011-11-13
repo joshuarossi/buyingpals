d70de48574db4898274138f67e186e0f73b06756
Tony Xiao <tonyx.ca@gmail.com>
2011-11-13 15:43:32 -0500

    making factual integrated

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from models import *
+import urllib
 
 def home(request):
     return render_to_response('base.html', context_instance=RequestContext(request))
@@ -14,6 +15,16 @@ def login(request):
 
 def logout(request):
     return HttpResponse('You are logged out')
+
+def factual(request):
+    bType = "carpet"
+    zipCode = "08540"
+    url = 'http://api.v3.factual.com/t/places.json?KEY=flUByXx9F8FQgB3tPIiam1t2Dn67cQ7r1J9rTz1r&filters='
+    filter = '{\"category":{\"$search\":\"' + bType + '\"},'
+    filter += ' \"postcode\":{\"$bw\":\"' + str(zipCode) + '\"}}'
+    url += urllib.quote(filter)
+    page = urllib.urlopen(url)
+    return HttpResponse(page.read())
 
 def signup(request):
     return render_to_response('signup.html', context_instance=RequestContext(request))
