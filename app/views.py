c79c34a3e5808d355594eb64f87dd5385e2352b2
Tony Xiao <tonyx.ca@gmail.com>
2011-11-13 11:34:27 -0500

    reverted out the heroku changes and NEW commit

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render_to_response
+
+def home(request):
+    return render_to_response('base.html', context_instance=RequestContext(request))
+
+def test(request):
+    return render_to_response('test.html', context_instance=RequestContext(request))
+
+def login(request):
+    return render_to_response('login.html', context_instance=RequestContext(request))
+
+def logout(request):
+    return HttpResponse('You are logged out')
+
+def signup(request):
+    return render_to_response('signup.html', context_instance=RequestContext(request))
+
+def comingsoon(request):
+	return render_to_response('comingsoon.html',  context_instance=RequestContext(request))
+
+def post(request):
+	return render_to_response('post.html',  context_instance=RequestContext(request))
+
+def confirm(request):
+	return render_to_response('confirm.html',  context_instance=RequestContext(request))
+
+def details(request):
+	return render_to_response('details.html',  context_instance=RequestContext(request))
+
+
+
+
